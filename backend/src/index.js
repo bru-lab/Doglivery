f77051e92d1ec5a2a93cb1c94f7d0904a76dce4a
bru-lab <brflmn132@gmail.com>
2026-09-09 10:26:22 -0300

    "Project completed"

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,12 +31,12 @@ app.use("/api/orders", orderRoutes);
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-if(process.env.NODE_ENV==="production"){
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res)=> {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  })
+  app.get("/{*splat}", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+  });
 }
 
 
